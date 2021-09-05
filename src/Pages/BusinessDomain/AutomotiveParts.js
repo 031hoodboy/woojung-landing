b671d9467adf85abd9cffe2a1ea07394b7340d39
hoodboy <littlepark0107@gmail.com>
2021-09-06 02:10:02 +0900

    feat: AutomotiveParts rework

--- src/Pages/BusinessDomain/AutomotiveParts.js
+++ src/Pages/BusinessDomain/AutomotiveParts.js
@@ -8,6 +8,12 @@ import BackgroundImg from '../../assets/businessdomainbackground.png';
 import {PageTitleWrpper, PageTitle, MainImgTitle, NavBarWrapper, MobileNavBarWrapper, NavBlock, SelectNavBlock, BlueSpan, GreenSpan, Arrow, MobileSelectNavBlock, MobileNavBlock } from '../../components/PageStyle';
 import styled from '@emotion/styled';
 import ProfileImg from '../../assets/automotiveparts.png';
+import AutomotivePartsImg1 from '../../assets/automotivepartsimg1.png';
+import AutomotivePartsImg2 from '../../assets/automotivepartsimg2.png';
+import AutomotivePartsImg3 from '../../assets/automotivepartsimg3.png';
+import AutomotivePartsImg4 from '../../assets/automotivepartsimg4.png';
+import AutomotivePartsImg5 from '../../assets/automotivepartsimg5.png';
+import AutomotivePartsImg6 from '../../assets/automotivepartsimg6.png';
 
 const AutomotiveParts = () => {
 
@@ -53,23 +59,50 @@ const AutomotiveParts = () => {
                     </MobileNavBarWrapper>
                 </NavBarWrapper>
             <PageTitleWrpper>
-                <PageTitle>친환경 자동차 부품</PageTitle>
+                <PageTitle>전기 자동차</PageTitle>
+                <SubTitle>・Electric Vehicle(EV) 자동차의 이해</SubTitle>
             </PageTitleWrpper>
-            <HelloWrapper>
-                    <Profile/>
-                    <Descrip>
-                        우정에이치앤씨(주)는 풍부한 경험을 겸비한 회사로써, 위험물안전관리법에 의거한 제조소등의 위치/구조 또는 설비에 적합하도록 <BlueSpan>설치허가 및 변경허가의 대행 업무</BlueSpan>를 해드리고 있습니다.
-                        <br/>
-                        <br/>
-                        설치허가 및 변경허가 접수 후, 완공검사 필증 교부까지 마무리 지어드립니다. 또한 고객의 작업장에 알맞도록 지정배수를 계산하여 품목변경도 해드리고 있으며 제조소 등의 <GreenSpan>용도폐지, 지위승계 신고 대행</GreenSpan>도 해드리고 있습니다.
-                        <br/>
-                        <br/>
-                        위험물의 인허가에 관한 신뢰할 수 있는 인허가를 약속드리며, 위험물 시설의 인허가 절차 등에 관한 많은 어려움을 가지고 있었을 것이라 여깁니다.
-                        <br/>
-                        <br/>
-                        당사는 <BlueSpan>위험물 설계, 시공, 완공까지 전반적인 종합컨설팅회사</BlueSpan>로써 항상 신뢰할 수 있는 서비스로 귀사의 부가가치 창출에 기여하겠습니다.
-                    </Descrip>
-                </HelloWrapper>
+            <ImgWrapper>
+                <Img1/>
+                <ImgContent>
+                    기존 석유를 이용한 내연기관을 탈피하여
+                    친환경적 에너지인 전기에너지를 바탕으로
+                    구동하는 자동차.
+                </ImgContent>
+            </ImgWrapper>
+            <ImgWrapperReverse>
+                <Img2/>
+                <ImgContent>
+                    전기자동차는 18세기에 제작되었으나, 기술의
+                    한계로 상용화가 되지 못하다가 최근 배기가스와 
+                    같은 공해문제로 인해 전 세계적으로 차세대 자동차 
+                    영역으로 접근하여 개발중에 있습니다.
+                </ImgContent>
+            </ImgWrapperReverse>
+            <ImgWrapper>
+                <Img3/>
+                <ImgContent>
+                당사는 전기자동차에 사용되는 배터리의 필수요소인 전압 패드를 생산 및 납품하는 전문 업체로써 차세대 자동차 완성에 일조하고 있습니다.
+                </ImgContent>
+            </ImgWrapper>
+            <PageTitleWrpper>
+                <SubTitle>・BMA(Battery Module Assembly) 구성</SubTitle>
+            </PageTitleWrpper>
+            <Img4/>
+            <ImgWrapperReverse>
+                <Img5/>
+                <ImgContent>
+                배터리셀은 전기에너지를 사용하는 리튬이온 배터리의 기본단위로 사각형의 케이스에 넣어 만든 배터리 시스템의 초기 구성단계
+                </ImgContent>
+            </ImgWrapperReverse>
+            <ImgWrapper>
+                <Img6/>
+                <ImgContent>
+                    배터리모듈은 배터리셀의 파손 방지를 위해 
+                    일정한 개수로 묶어 프레임에 넣은 중요조립체
+                </ImgContent>
+            </ImgWrapper>
+            <Margin/>
             <Footer/>
         </PageWrapper>
     );
@@ -143,4 +176,110 @@ const Profile = styled.div`
   }
 `;
 
+const SubTitle = styled.div`
+    color: #0A109F;
+    font-size: 30px;
+    margin-top: 10vh;
+    font-weight: 600;
+    @media screen and (max-width: 780px) {
+        width: 90vw;
+        font-size: 18px;    
+  }
+`;
+
+
+const ImgWrapper = styled.div`
+    margin: 10vh auto;
+    display: flex;
+    flex-wrap: wrap;
+    width: 90vw;
+    max-width: 1088px;
+    justify-content: space-between;
+    flex-direction: row-reverse;
+    @media screen and (max-width: 1155px) {
+        justify-content: center;
+  }
+  @media screen and (max-width: 780px) {
+        font-size: 16px;    
+        margin: 5vh auto 5vh auto;
+  }
+`;
+
+const ImgWrapperReverse = styled(ImgWrapper)`
+    flex-direction: row;
+`;
+
+const ImgContent = styled.div`
+    width: 295px;
+    margin: auto 0;
+    line-height: 35px;
+    @media screen and (max-width: 1155px) {
+        margin-top: 2vh;
+        max-width: 640px;
+        width: 90vw;
+}
+`;
+
+const Img1 = styled.div`
+    width: 674px;
+    height: 360px;
+    background-image: url(${AutomotivePartsImg1});
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center; 
+    display: flex;
+    @media screen and (max-width: 840px) {
+        max-width: 640px;
+        width: 90vw;
+        height: 60vw;
+  }
+`;
+
+const Img2 = styled(Img1)`
+    background-image: url(${AutomotivePartsImg2});
+`;
+
+const Img3 = styled(Img1)`
+    background-image: url(${AutomotivePartsImg3});
+`;
+
+
+const Img4 = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    width: 90vw;
+    max-width: 1088px;
+    height: 20vw;
+    background-image: url(${AutomotivePartsImg4});
+    margin: 15vh auto 0 auto;
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-position: center; 
+    @media screen and (max-width: 1155px) {
+        width: 90vw;
+        max-width: 674px;
+        height: 20vw;
+        margin-top: 5vh;
+  }
+  @media screen and (max-width: 840px) {
+    max-width: 640px;
+        width: 90vw;
+        height: 20vw; 
+     }
+`;
+
+const Img5 = styled(Img1)`
+    background-image: url(${AutomotivePartsImg5});
+     border: 1px solid #D3D3D3;
+`;
+
+const Img6 = styled(Img1)`
+    background-image: url(${AutomotivePartsImg6});
+    border: 1px solid #D3D3D3;
+`;
+
+const Margin = styled.div`
+    width: 100%;
+     height: 20vh;
+`;
 export default AutomotiveParts;
